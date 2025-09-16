46de78d676d85b43181f303216943f9c78ae3d08
Sleezzi <contact@sleezzi.fr>
2025-09-16 14:57:28 +0200

    Bug fixed

--- init.sh
+++ init.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade
 sudo apt full-upgrade
 
-sudo apt install ufw nginx git fail2ban # Install ufw, nginx, git & fail2ban
+sudo apt install ufw git fail2ban # Install ufw, nginx, git & fail2ban
 sudo apt autoremove # Remove unused file
 sudo apt autoclean # Remove unused file
 
@@ -19,7 +19,8 @@ sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \ $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin && \
+sudo groupadd docker
 
 
 # Re-update apt-get
@@ -28,14 +29,15 @@ sudo apt-get update
 sudo apt-get autoclean
 
 # Add alias
-sudo echo "alias cls=clear" >> /etc/bash.bashrc
-sudo echo "alias ufw=\"sudo ufw\"" >> /etc/bash.bashrc
+sudo echo "alias cls=clear" >> /etc/bash.bashrc && \
+sudo echo "alias ufw=\"sudo ufw\"" >> /etc/bash.bashrc && \
 source /etc/bash.bashrc # Apply the change on this terminal, useless because it will be closen by the reboot
 
 # Change the ssh port
 sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.old # Change the port in the config file
-wget https://sleezzi.fr/VPS-Init/sshd_config && sudo mv sshd_config /etc/ssh/sshd_config # Change the port in the config file
-sudo systemctl daemon-reload
+wget https://sleezzi.fr/VPS-Init/sshd_config && \
+sudo mv sshd_config /etc/ssh/sshd_config # Change the port in the config file
+sudo systemctl daemon-reload && \
 sudo systemctl restart ssh.socket
 
 # Config fail2ban
@@ -45,6 +47,7 @@ sudo systemctl restart fail2ban
 # Setup the firwall
 sudo ufw deny 22 comment "Close the default ssh port to avoid brute force attack from bot"
 sudo ufw allow 50150/tcp comment "The new port for ssh connection"
+sudo apt install nginx && \
 sudo ufw allow "Nginx Full" comment "Allow nginx"
 sudo ufw enable
 
@@ -56,7 +59,6 @@ sudo adduser default --no-create-home
 sudo mkdir /home/sleezzi/pool
 sudo chmod a-wrx u+rwx g+wr /home/sleezzi/pool
 
-sudo groupadd docker
 sudo usermod -aG docker sleezzi
 sudo usermod -aG docker ubuntu
 
